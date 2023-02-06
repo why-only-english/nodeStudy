fff3005af3ab6a935c68f5e4d2bc1e243edd14cc
jgmoon <6813821@tukorea.ac.kr>
2023-02-06 16:12:52 +0900

    Create sever program

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-// events 코어 모듈
-const EvenEmitter = require('events');
+// http(통신규약, 프로토콜) 코어 모듈
+const http = require('http');
 
-// EvenEmitter 클래스로 myEmitter 객체 생성
-const myEmitter = new EvenEmitter;
-
-const obj = {type: 'text', data: 'hello', date: '2023-02-06'};
-
-// on 메소드 호출, 이벤트 발생했을때 실행할 콜백 등록
-myEmitter.on('test', (info) => {
-    console.log(info);
-})
-
-// emit 메소드 호출, 발생시킬 이벤트
-myEmitter.emit('test', obj);
+// createServer 메소드 : server 역할을 하는 객체 하나를 생성
+let server = http.createServer();
+server.listen(3000);
